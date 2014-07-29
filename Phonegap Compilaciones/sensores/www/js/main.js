3858f033e80f1d5966486aeaa1e1e2defd5dd12b
Giovanny Suarez <suarezkrac@gmail.com>
2014-07-29 16:51:51 -0500

    nuevos archivos
    nuevos archivos

--- Phonegap Compilaciones/sensores/www/js/main.js
+++ Phonegap Compilaciones/sensores/www/js/main.js
@@ -1,6 +1,5 @@
-//$(document).on("mobileinit", appReady);
-//$(document).on("ready", appReady);
-document.addEventListener("deviceready",appReady,false);
+document.addEventListener("ready",appReady,false);
+//document.addEventListener("deviceready",appReady,false);
 var contrasena;
 var usuario;
 var tamanoPantalla = $(window).width();
@@ -10,21 +9,67 @@ function appReady () {
 	pictureSource=navigator.camera.PictureSourceType;
    	destinationType=navigator.camera.DestinationType;
 	$('#boton_1').on("click", capturePhoto);
+	$('#boton_2').on("click", capturePhotoEditable);
+	$('#boton_3').on("click", capturePhotoLibrary);
+	$('#boton_4').on("click", capturePhotoAlbum);
 	$("#ingresar").on("click", validarUsuario);
 	if (tamanoPantalla>480){
 		cambiarListaPantalla1();
 	}
 	$(window).on("resize", cambiarListaPantalla1);
 }
+$(document).on("pagecreate", "#gps", function() {
+	var defaultLatLng = new google.maps.LatLng(34.0983425, -118.3267434);  
+    if (navigator.geolocation) {
+    	alert("sensor existe");
+    	function success (pos) {
+    		alert("sensor success");
+    		var ubicacion = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);  
+    		drawMap(ubicacion);
+    	}
+    	function fail (error) {
+    		drawMap(defaultLatLng);
+    	}
+    	navigator.geolocation.getCurrentPosition(success, fail);
+    }else{
+    	alert("sensor no existe");
+    	drawMap(defaultLatLng);
+    }
+    function drawMap(latlng) {
+        var myOptions = {
+            zoom: 18,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+    }
+});
+//CSS #gps, #map-canvas { width: 100%; height: 100%; padding: 0; }
+
+function capturePhotoLibrary () {
+	navigator.camera.getPicture(onPhotoSuccessFile, onFail, { quality: 50,destinationType: destinationType.FILE_URI, sourceType: pictureSource.PHOTOLIBRARY });
+}
+function capturePhotoAlbum () {
+	navigator.camera.getPicture(onPhotoSuccessFile, onFail, { quality: 50,destinationType: destinationType.FILE_URI, sourceType: pictureSource.SAVEDPHOTOALBUM });
+}
+function onPhotoSuccessFile (imageURI) {
+	
+	$('#largeImage').attr("src", imageURI);
+}
+function capturePhotoEditable () {
+	//BASE 64 Encoded
+	navigator.camera.getPicture(onPhotoSuccess, onFail, { quality: 50, allowEdit: true, destinationType: destinationType.DATA_URL });
+	}
 function capturePhoto () {
 	//BASE 64 Encoded
 	navigator.camera.getPicture(onPhotoSuccess, onFail, { quality: 50,destinationType: destinationType.DATA_URL });
 	alert("foto capturada");
 }
+
+
 function onPhotoSuccess (image) {
 	alert("exitoso");
 	$('#smallImage').attr("src", "data:image/jpeg;base64,"+image);
-
 }
 function onFail (message) {
 	alert("Error en : "+message);
